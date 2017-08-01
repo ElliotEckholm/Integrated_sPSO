36de8128ad7f040f3fca1355576d2e7b6c9e03cc
ElliotEckholm <ellioteckholm@gmail.com>
2017-08-01 17:27:00 -0500

    Delete compileCommands.sh

--- sPSO_Integrated_With_MohantyCode/compileCommands.sh
+++
@@ -1,6 +0,0 @@
-gcc -c ptapso.c -I/usr/local/include
-gcc -c maxphaseutils.c -I/usr/local/include
-gcc -c ptapsotestfunc.c -I/usr/local/include
-gcc -c test_ptapso.c -I/usr/local/include
-gcc ptapso.o maxphaseutils.o ptapsotestfunc.o test_ptapso.o -L/usr/local/lib -lgsl -lgslcblas -lm -o test_ptapso.exe
-./test_ptapso.exe
